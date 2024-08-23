e24c4e6a37b022e40363564590c0c7dbe558e880
Ilya_Sedelnikov <illyasedelnikov123@gmail.com>
2024-08-23 12:19:09 +0200

    optimized dependencies in modules

--- user-service/src/main/java/com/project/userservice/users/services/UserService.java
+++ user-service/src/main/java/com/project/userservice/users/services/UserService.java
@@ -1,14 +1,11 @@
 package com.project.userservice.users.services;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.userservice.users.data.User;
 import com.project.userservice.users.data.dto.UserRequest;
 import com.project.userservice.users.data.dto.UserResponse;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,7 +15,6 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final ModelMapper modelMapper;
-    private final ObjectMapper objectMapper;
 
     public UserResponse saveUser(UserRequest userRequest) {
         User mappedUser = modelMapper.map(userRequest, User.class);
@@ -31,17 +27,6 @@ public class UserService {
         User obtainedUser = getUserById(userId);
 
         return modelMapper.map(obtainedUser, UserResponse.class);
-    }
-
-    @SneakyThrows
-    @KafkaListener(topics = "update-user-balance", groupId = "user-service")
-    public void updateUserBalance(String userModel) {
-        log.info(userModel);
-        User user = objectMapper.readValue(userModel, User.class);
-        User obtainedUser = getUserById(user.getId());
-
-        obtainedUser.setBalance(user.getBalance());
-        userRepository.save(obtainedUser);
     }
 
     public void updateUserBalance(String userId, long newBalance) {
